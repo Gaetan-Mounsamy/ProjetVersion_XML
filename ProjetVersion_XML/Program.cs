7e0f56990736b234c977dadd63168ff0f61382f5
Gaetan <gaetan.mounsamy@efrei.net>
2023-01-17 10:36:14 +0100

    Recuperation des données du XML

--- ProjetVersion_XML/Program.cs
+++ ProjetVersion_XML/Program.cs
@@ -11,21 +11,33 @@ namespace ProjetVersion_XML
     {
         static void Main(string[] args)
         {
+            
+                /*
             XmlDocument doc = new XmlDocument();
             doc.Load(@"C:\\Users\gaeta\Desktop\Fun\xml_file.xml");
             //XML_Format.DataForm(doc);
-            
-            Console.WriteLine("Hello World!");
+                */
 
             var watch = Stopwatch.StartNew();
 
-            System.Xml.Serialization.XmlSerializer xmlSerializer = new XmlSerializer(typeof(Export));
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Export));
             using System.IO.StreamReader streamReader = new System.IO.StreamReader(@"C:\Users\gaeta\Desktop\Fun\formattedFile.xml");
-            var objt =(Export) xmlSerializer.Deserialize(streamReader);
+                
+            //Casting operation, permet de convertir le resultat de Deserialize
+                // Dans ce cas, la méthode Deserialize renvoie un objet, et cet objet est converti au type Export.
+                // Cette opération permet d'accéder à l'objet et de le manipuler en tant qu'instance de la classe
+                // Export, plutôt que comme un simple objet général.
+            var objt = (Export) xmlSerializer.Deserialize(streamReader);
+                //deserialize the XML data into an object of type Export
             watch.Stop();
             Console.WriteLine(watch.ElapsedMilliseconds);
 
-            var offre601 = objt.StandardSalesItem.FirstOrDefault(x => x.Name == "601-TDG-NIVEAU-1");
+            var filtre = objt.StandardSalesItem.FirstOrDefault(x => x.Name == "601-TDG-NIVEAU-1");
+            //Console.WriteLine(filtre.Links.Link[0].Child.Name);
+            
+            
+            //using System.IO.StreamReader doubleOuverture = new System.IO.StreamReader(@"C:\Users\gaeta\Desktop\Fun\test.xml");
+
         }
     }
 }
