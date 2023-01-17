7457a1e0a8a6dbf7697b342ffa8149fe4f36d480
Gaetan <gaetan.mounsamy@efrei.net>
2023-01-17 14:23:25 +0100

    Recuperation des données du XML

--- ProjetVersion_XML/Program.cs
+++ ProjetVersion_XML/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Xml;
@@ -27,6 +28,8 @@ namespace ProjetVersion_XML
                 // Dans ce cas, la méthode Deserialize renvoie un objet, et cet objet est converti au type Export.
                 // Cette opération permet d'accéder à l'objet et de le manipuler en tant qu'instance de la classe
                 // Export, plutôt que comme un simple objet général.
+                
+
             var objt = (Export) xmlSerializer.Deserialize(streamReader);
                 //deserialize the XML data into an object of type Export
             watch.Stop();
@@ -35,9 +38,26 @@ namespace ProjetVersion_XML
             var filtre = objt.StandardSalesItem.FirstOrDefault(x => x.Name == "601-TDG-NIVEAU-1");
             //Console.WriteLine(filtre.Links.Link[0].Child.Name);
             
+/*
+            var testons = filtre.Links;
+            testons.Link[0].Child.Name = "COUCOU DU CON";
+            testons.Link[1].Child.Name = "CONNARD VA";
             
-            //using System.IO.StreamReader doubleOuverture = new System.IO.StreamReader(@"C:\Users\gaeta\Desktop\Fun\test.xml");
+            
+            
+            StandardSalesItem salesItem = new StandardSalesItem();
+            salesItem.Name = "c'est des test, tqt";
+            salesItem.Eligibility = "true";
+            salesItem.Links = new Links();
+            salesItem.Links.Link = new List<Link>();
+            salesItem.Links.Link.Child = new Child();
+            salesItem.Links.Link[0].Child.Name = "COUCOU DU CON";
+            
 
+            StandardSalesItem salesItem = new StandardSalesItem();
+            salesItem.Name = "coucouc test";
+            salesItem.Workspace = "auto";
+            */
         }
     }
 }
